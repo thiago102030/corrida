9122898b45b4a9d42ade8c2d9e2f667cba4359d8
thiago102030 <161844372+thiago102030@users.noreply.github.com>
2025-03-21 08:05:03 -0300

    s

--- sketch.js
+++ sketch.js
@@ -1,37 +1,36 @@
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(400, 400);
+}
+
+let xJogador1 = 0;
+let xJogador2 = 0;
+
+function draw() {
+  if (focused == true) {
+    background("#D2EBB5");
+  } else {
+    background("rgb(238,178,178)");
   }
-  
-  let xJogador1 = 0;
-  let xJogador2 = 0;
-  
-  function draw() {
-    if (focused == true) {
-      background("#D2EBB5");
-    } else {
-      background("rgb(238,178,178)");
-    }
-  
-    textSize(40);
-    text(";-;", alberto, 100);
-    text(".-.", malro, 300);
-    rect(350, 0, 10, 400);
-    if (xJogador1 > 350) {
-      text("alberto venceu!", 50, 200);
-      noLoop();
-    }
-    if (xJogador2 > 350) {
-      text("malro 2 venceu!", 50, 200);
-      noLoop();
-    }
+
+  textSize(40);, 
+  text(";-;", marlon, 140);
+  text("+-+", alfredo, 250);
+  rect(350, 0, 10, 400);
+  if (xJogador1 > 350) {
+    text("marlon venceu!", 50, 200);
+    noLoop();
   }
-  
-  function keyReleased() {
-    if (key == "w") {
-      xJogador1 += random(20);
-    }
-    if (key == "q") {
-      xJogador2 += random(20);
-    }
+  if (xJogador2 > 350) {
+    text("alfredo venceu!", 50, 200);
+    noLoop();
   }
-  
+}
+
+function keyReleased() {
+  if (key == "q") {
+    xJogador1 += random(20);
+  }
+  if (key == "e") {
+    xJogador2 += random(20);
+  }
+}
